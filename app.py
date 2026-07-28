76f468833ba3affe1a4455648d7146ba087b2e00
Flavio Alves <alveslimneto@gmail.com>
2026-07-28 00:05:32 -0400

    pandas test

--- app.py
+++ app.py
@@ -7,17 +7,6 @@ data_br = data_atual.strftime("%d/%m/%Y")
 # print(data_br)
 data = "27/07/2026"
 df = pd.read_csv("dado.csv")
-resultado = df[df["data_prevista"] == "27/07/2026"]
-if not resultado.empty:
-    # Índice da linha no DataFrame (0-indexed)
-    linha_df = resultado.index[0]
-    nome_coluna = "status"
-
-    # Salva a posição/referência na variável
-    posicao = {"linha_index": linha_df, "coluna": nome_coluna}
-
-    # Acessa o valor nessa posição exata
-    valor_exato = df.at[linha_df, nome_coluna]
-    print(
-        f"Posição no DF: linha {linha_df}, coluna '{nome_coluna}' -> Valor: {valor_exato}"
-    )
+filterdata = df["data_prevista"] == data_br
+filterop = df["op"] > 38000
+print(df[filterdata & filterop])
