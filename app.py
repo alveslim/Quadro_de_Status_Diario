7084048e095fa71c7317ef0989155948ec7d69c6
Flavio Alves <alveslimneto@gmail.com>
2026-07-28 23:48:11 -0400

    teste

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from datetime import date
 import pandas as pd
 
+# Data atual Br
 data_atual = date.today()
-# print(data_atual)
 data_br = data_atual.strftime("%d/%m/%Y")
-# print(data_br)
-data = "27/07/2026"
+# lendo planilha
 df = pd.read_csv("dado.csv")
+# filtrando apenas planilha de 38000 em diante && data atual
 filterdata = df["data_prevista"] == data_br
 filterop = df["op"] > 38000
+# apresentando o nosso filtro
 print(df[filterdata & filterop])
