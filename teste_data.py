34ec018767e52f2f0e06c20a5347d99babbebf09
Flavio Alves <alveslimneto@gmail.com>
2026-07-29 11:58:13 -0400

    feature: databr_amanha

--- teste_data.py
+++ teste_data.py
@@ -1,10 +1,12 @@
-from datetime import date
+from datetime import date, timedelta
 
 data_atual = date.today()
-print(data_atual)
+data_amanha = data_atual + timedelta(days = 1)
 data_br = data_atual.strftime("%d/%m/%Y")
+data_br_amanha = data_amanha.strftime("%d/%m/%Y")
 print(data_br)
-data = "27/07/2026"
+print(data_br_amanha)
+data = "30/07/2026"
 
 if data_br == data:
     print("é hoje")
